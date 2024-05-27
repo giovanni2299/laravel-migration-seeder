a5a9a3dfcc93d269f2701372b62cf5207695d755
giovanni2299 <giovanni-sorrentino@hotmail.it>
2024-05-27 16:49:49 +0200

    ADD: ccheking the date time

--- database/seeders/Trainsseeder.php
+++ database/seeders/Trainsseeder.php
@@ -16,17 +16,20 @@ class Trainsseeder extends Seeder
     public function run(Faker $faker): void
     {
         DB::table('trains')->truncate();
+
+        $company = ['Italo', 'Trenitalia'];
+
         for($i = 0; $i < 50; $i++){
             $newTrain = new Trains();
-            $newTrain->company = $faker->word();
+            $newTrain->company = $faker->randomElement($company);
             $newTrain->departure_station = $faker->city();
             $newTrain->arrival_station = $faker->city();
-            $newTrain->departure_time = $faker->dateTime();
-            $newTrain->arrival_time = $faker->dateTime();
-            $newTrain->train_code = $faker->bothify('?????-#####');
+            $newTrain->departure_time = $faker->dateTimeBetween('-1 day','now');
+            $newTrain->arrival_time = $faker->dateTimeBetween('now', '+1 day');
+            $newTrain->train_code = $faker->regexify('[A-Z]{2}[0-9]{3}[A-Z]{3}[0-9]{1}');
             $newTrain->coach_number = $faker->numberBetween(1, 11);
-            $newTrain->delay = $faker->boolean();
-            $newTrain->deleted = $faker->boolean();
+            $newTrain->delay = $faker->boolean(0);
+            $newTrain->deleted = $faker->boolean(0);
             $newTrain->save();
 
         }
